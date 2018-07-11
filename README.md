7d8d92e9340684b64a8678ac4a0506439097fde6
Matt Pewsey <mattpewsey@gmail.com>
2018-07-10 23:23:47 -0400

    Add CI, docs, and coverage

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Error Solver
 
+[![Build Status](https://travis-ci.com/line-mind/error_solver.svg?branch=master)](https://travis-ci.com/line-mind/error_solver)
+[![Documentation Status](https://readthedocs.org/projects/error-solver/badge/?version=latest)](https://error-solver.readthedocs.io/en/latest/?badge=latest)
+[![codecov](https://codecov.io/gh/line-mind/error_solver/branch/master/graph/badge.svg)](https://codecov.io/gh/line-mind/error_solver)
+
 ## Purpose
 
 This package contains a class for estimating the error tolerances for
--- setup.py
+++ setup.py
@@ -1,14 +1,24 @@
+import os
+from glob import glob
 from setuptools import setup, find_packages
 
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
 setup(name = 'error_solver',
-      version = '0.0.2',
-      description = '',
-      url = '',
-      license = '',
+      version = '0.0.0',
+      author = 'Matt Pewsey',
+      description = 'Estimates error tolerances for systems of equations based on the total derivatives',
+      long_description = long_description,
+      url = 'https://github.com/line-mind/error_solver',
+      license = 'BSD 3-Clause License',
       packages = find_packages(),
       include_package_data = True,
+      scripts = [],
       install_requires = ['numpy', 'sympy'],
       dependency_links = [],
-      keywords = [],
-      classifiers = [],
-      python_requires = '')
+      keywords = ['error-estimation', 'measurement-tolerances', 'error-tolerances'],
+      classifiers = ['Programming Language :: Python :: 3',
+                     'Operating System :: OS Independent',
+                     'License :: OSI Approved :: BSD License'],
+      python_requires = '>=2.7, >=3.4')
