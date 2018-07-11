adb01f11bc68bd4c8e5cd9d4e47ef842acfbbc24
Matt Pewsey <mattpewsey@gmail.com>
2018-07-10 23:41:45 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,4 +21,4 @@ setup(name = 'error_solver',
       classifiers = ['Programming Language :: Python :: 3',
                      'Operating System :: OS Independent',
                      'License :: OSI Approved :: BSD License'],
-      python_requires = '>=2.7.0, >=3.5.0')
+      python_requires = '>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*')
